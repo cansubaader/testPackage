8bc00013aa5c77b393c27c93ea152b8faf728f2a
Cansu <c.baader@hotmail.de>
2023-07-21 19:29:39 +0200

    package

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,9 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+#' @title possible travel destinations
+#' @param travel_destinations_cities is a list of cities
+#' @returns names of cities as possible destinations
+#'
+destinations <- function() {
+  travel_destinations_cities <- c("london", "paris", "Amsterdam", "Lissabon")
+  return(travel_destinations_cities)
 }
